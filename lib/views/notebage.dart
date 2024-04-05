f7902beaef3ec3d91ddd05512444ba2ebe0f388f
ammar mansor <ammarmansor2002@gmail.com>
2024-04-05 20:47:06 +0300

    customappbare

---
+++ lib/views/notebage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:noteapp/views/widgets/custom_appbare.dart';
+
+class Notepage extends StatelessWidget {
+  const Notepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(children: [
+        Customappbare(),
+      ]),
+    );
+  }
+}
